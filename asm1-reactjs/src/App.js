cc690adffbdd87268f1844e56f131c22f2d20f45
Your Name <you@example.com>
2022-02-23 08:32:24 +0700

    start coding StaffListComponent.js

---
+++ asm1-reactjs/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import {DEPARTMENTS, ROLE, STAFF} from './staffs.jsx';
+import StaffList from './components/StaffListComponent';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       
+      </header>
+    </div>
+  );
+}
+
+export default App;
