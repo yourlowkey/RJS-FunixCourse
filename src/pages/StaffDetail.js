ccdacfc98b2b325a0ccae984306ac08b24ffd4c0
yourlowkey <m.phuc2552000@gmail.com>
2022-10-07 09:32:16 +0700

    fix staffdetail page

--- src/pages/StaffDetail.js
+++ src/pages/StaffDetail.js
@@ -2,17 +2,23 @@ import React from 'react';
 import { useParams } from 'react-router-dom';
 import {STAFFS} from '../data/StaffData';
 const StaffDetail = () => {
-  const {staffId} = useParams();
-  const staff = STAFFS.find((staff) => staff.id === staffId);
+  const { staffId } = useParams();
+  const staff = STAFFS.find(staff => staff.id === parseInt(staffId));
+  console.log(staff);
+  console.log(typeof (staffId));
+  function isoDate(dateISO8601) {
+    var d = new Date(dateISO8601);
+    return d.getUTCDate() + '/' + (d.getUTCMonth() + 1) + '/' + d.getUTCFullYear();
+ }
   return (
     <div>
-      <img src={staff.image} alt="staff image" />
-      <h1>{'Ho va ten :' + staff.name}</h1>
-      <p>{'Ngay sinh' + staff.doB} </p>
-      <p>{'Ngay vao cong ty :' + staff.startDate}</p>
-      <p>{'Phong ban :' + staff.departments.name} </p>
-      <p>{'So ngay nghi :' + staff.annualLeave}</p>
-      <p>{'So ngay da lam them :' + staff.overTime}</p>
+      {/* <img src={staff.image} alt="staff image" /> */}
+      <h1>{`Họ và tên : ${staff.name}`}</h1>
+      <p>{'Ngày sinh : ' + isoDate(staff.doB)} </p>
+      <p>{'Ngày vào công ty : ' + isoDate(staff.startDate)}</p>
+      {/* <p>{'Phong ban :' + staff.departments.name} </p> */}
+      <p>{'Số ngày nghỉ : ' + staff.annualLeave}</p>
+      <p>{'Số ngày đã làm thêm : ' + staff.overTime}</p>
     </div>
   );
 };
--- src/pages/StaffList.js
+++ src/pages/StaffList.js
@@ -4,18 +4,23 @@ import { Link } from 'react-router-dom';
 const StaffList = () => {
   var staffList = [];
   staffList = STAFFS.map((item) => (
-    <div className="card">
-      <img className="card-img-top mt-3" src={item.image} alt="Card image" />
-      <div>
-        <h4 className="card-item">{item.name}</h4>
-        <p>
-          <td>
-            <Link to={`/staff/${item.id}`}>Details</Link>
-          </td>
-        </p>
+      <div className="card col-md-4 col-sm-12 mb-3">
+        <img className="card-img-top mt-3" src={item.image} alt="Card image" />
+        <div>
+          <h4 className="card-item">{item.name}</h4>
+          <p>
+            <td>
+              <Link to={`/staff/${item.id}`}>Details</Link>
+            </td>
+          </p>
+        </div>
       </div>
-    </div>
   ));
-  return staffList;
+  return (
+    <div className='row'>
+        {staffList}
+    </div>
+  )
+  
 };
 export default StaffList;
