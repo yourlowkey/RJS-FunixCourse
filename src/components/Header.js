91bdf14e0608f6fe2a77320a53e724ba02b9f166
yourlowkey <m.phuc2552000@gmail.com>
2022-10-22 15:51:45 +0700

    add header and footer to routes

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,28 +5,33 @@ import { Link, NavLink } from 'react-router-dom';
 
 import { Navbar, Nav } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
 const Header = () => {
   return (
-    <>
+    <div className="Header">
       <Navbar className="bg-primary">
         <Nav>
           <Nav.Link as={NavLink} to="/" exact="true">
             Home
           </Nav.Link>
           <Nav.Link as={NavLink} to="/staff">
-            Nhan vien
+          <FontAwesomeIcon icon="fa-solid fa-user-group" />          
+           Nhan vien
           </Nav.Link>
           <Nav.Link as={NavLink} to="/department">
+          <FontAwesomeIcon icon="fa-solid fa-building-user" />
             Phong ban
           </Nav.Link>
           <Nav.Link as={NavLink} to="/salary">
+          <FontAwesomeIcon icon="fa-solid fa-money-bill" />
             Bang luong
           </Nav.Link>
         </Nav>
       </Navbar>
 
       <Outlet />
-    </>
+    </div>
   );
 };
 
--- src/pages/App.js
+++ src/pages/App.js
@@ -7,18 +7,20 @@ import StaffDetail from './StaffDetail';
 import SalaryTable from './SalaryTable';
 import Home from './Home';
 import { Route, BrowserRouter, Routes } from 'react-router-dom';
+import Footer from '../components/Footer';
 export default function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Header />}>
+    <Header></Header>
+      {/* <Route path="/" element={<Header />} /> */}
+      <Routes className="Main">
           <Route index element={<Home />} />
           <Route path="staff" element={<StaffList />} />
           <Route path="staff/:staffId" element={<StaffDetail />} />
           <Route path="salary" element={<SalaryTable />} />
           <Route path="department" element={<DepartmentList />} />
-        </Route>
       </Routes>
+      <Footer></Footer>
     </BrowserRouter>
   );
 }
